6924fd7839dc14679d2cb2e372707bd49066944a
osu <joanosuna9@gmail.com>
2021-11-10 21:04:25 +0100

    Added registerRequestSchema with Joi

--- database/requestSchemas/registerRequestSchema.js
+++ database/requestSchemas/registerRequestSchema.js
@@ -2,8 +2,10 @@ const { Joi } = require("express-validation");
 
 const loginRequestSchema = {
   body: Joi.object({
+    name: Joi.string().required(),
     username: Joi.string().required(),
     password: Joi.string().required(),
+    admin: Joi.boolean().optional(),
   }),
 };
 
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -4,11 +4,7 @@ const {
   loginRequestSchema,
 } = require("../../database/requestSchemas/loginRequestSchema");
 const createUser = require("../middlewares/createUser");
-const { validate } = require("express-validation");
 const loginUser = require("../controller/userController");
-
-const { loginRequestSchema } =
-  "../../database/models/requestSchemas/loginUserSchema";
 
 const router = express.Router();
 
