41fe569f5c15438e707d45dd8289963d704c1e5e
osu <joanosuna9@gmail.com>
2021-11-10 20:58:05 +0100

    Refractored createUser function

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const { validate } = require("express-validation");
+const {
+  loginRequestSchema,
+} = require("../../database/requestSchemas/loginRequestSchema");
 const createUser = require("../middlewares/createUser");
 
 const router = express.Router();
 
 router.post("/login");
 
-router.post("/register", createUser);
+router.post("/register", validate(loginRequestSchema), createUser);
 
 module.exports = router;
