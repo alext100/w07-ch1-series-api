9c4c3ad65846446821ef3f642547bddb5a37c8e8
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-10 18:22:30 +0100

    Index.js with initializeServer function created

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const chalk = require("chalk");
+const cors = require("cors");
+const debug = require("debug")("robots:server");
+const express = require("express");
+const morgan = require("morgan");
+const {
+  notFoundErrorHandler,
+  generalErrorHandler,
+} = require("./middlewares/error");
+const platformRoutes = require("./routes/platformRoutes");
+const usersRoutes = require("./routes/usersRoutes");
+const seriesRoutes = require("./routes/seriesRoutes");
+
+const app = express();
+
+app.use(cors());
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+const initializeServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.yellow(`Server is listening on port number: ${port}`));
+      resolve(server);
+    });
+
+    server.on("error", (error) => {
+      debug(chalk.red("There was an error starting the server"));
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.red(`The port ${port} is in use.`));
+      }
+      reject();
+    });
+
+    server.on("close", () => {
+      debug(chalk.yellow("Server express disconnected"));
+    });
+  });
+
+app.use("/users", usersRoutes);
+app.use("/platforms", platformRoutes);
+app.use("/series", seriesRoutes);
+
+app.use(notFoundErrorHandler);
+app.use(generalErrorHandler);
+
+module.exports = { app, initializeServer };
