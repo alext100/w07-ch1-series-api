e20cd438f09486682d5d19040ac65e49402d56d9
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-11 19:38:27 +0100

    Tests for updateSerie function added

--- server/controller/serieController.test.js
+++ server/controller/serieController.test.js
@@ -1,7 +1,5 @@
 const Serie = require("../../database/models/serie");
-const createSerie = require("./serieController");
-
-jest.mock("../../database/models/serie");
+const { createSerie, updateSerie } = require("./serieController");
 
 describe("Given a createSerie function", () => {
   describe("When it receives a serie", () => {
@@ -50,6 +48,57 @@ describe("Given a createSerie function", () => {
 
       expect(next).toHaveBeenCalledWith(error);
       expect(error.code).toBe(400);
+    });
+  });
+});
+
+describe("Given an updateSerie function", () => {
+  describe("When it receives an updated serie", () => {
+    test("Then it should invoke res.json with this serie", async () => {
+      const updatedSerie = {
+        id: "6185c1af9f1964f08e62d131",
+        name: "Cuéntame...",
+        img: "url.jpg",
+        seen: false,
+        platform: "618c2bdc9a1dff86b9be0156",
+      };
+      const req = {
+        body: updatedSerie,
+      };
+      const { id } = req.body;
+      console.log("id: ", id);
+      const res = {
+        json: jest.fn(),
+      };
+
+      Serie.findByIdAndUpdate = jest.fn().mockResolvedValue(updatedSerie);
+      await updateSerie(req, res, null);
+
+      expect(res.json).toHaveBeenCalledWith(updatedSerie);
+    });
+  });
+
+  describe("When it receives a non existent serie", () => {
+    test("Then it should invoke a next function with a 404 error", async () => {
+      Serie.findByIdAndUpdate = jest.fn().mockResolvedValue(null);
+      const req = {
+        body: {
+          id: "6222d83be45c3a8801f1440d",
+        },
+      };
+      const next = jest.fn();
+      const expectedError = {
+        code: 404,
+        message: "Serie not found",
+      };
+
+      await updateSerie(req, null, next);
+
+      expect(next.mock.calls[0][0]).toHaveProperty(
+        "message",
+        expectedError.message
+      );
+      expect(next.mock.calls[0][0]).toHaveProperty("code", expectedError.code);
     });
   });
 });
