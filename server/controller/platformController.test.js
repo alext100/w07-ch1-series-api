f7ce9ef6e30dd10fa11826b3db02d900cb69e64b
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-11 11:02:11 +0100

    Test for createPlatform for reject added

--- server/controller/platformController.test.js
+++ server/controller/platformController.test.js
@@ -1,6 +1,8 @@
 const Platform = require("../../database/models/platform");
 const createPlatform = require("./platformController");
 
+jest.mock("../../database/models/platform");
+
 describe("Given a createPlatform function", () => {
   describe("When it receives a platform", () => {
     test("Then it should invoke res.json() with the platform", async () => {
@@ -22,6 +24,27 @@ describe("Given a createPlatform function", () => {
       await createPlatform(req, res, null);
 
       expect(res.json).toHaveBeenCalledWith(platform);
+    });
+  });
+
+  describe("And Platform.create rejects", () => {
+    test("Then it should invoke next function with error rejected", async () => {
+      const error = {};
+      Platform.create = jest.fn().mockRejectedValue(error);
+      const req = {
+        body: {
+          platform: {
+            name: "N",
+            img: "q",
+          },
+        },
+      };
+      const res = {};
+      const next = jest.fn();
+
+      await createPlatform(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(error);
     });
   });
 });
