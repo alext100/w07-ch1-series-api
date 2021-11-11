cab9147177a795ccb268126c1146d9c0d0a880e5
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-11 19:35:53 +0100

    updateSerie function to serieController added

--- server/controller/serieController.js
+++ server/controller/serieController.js
@@ -12,4 +12,24 @@ const createSerie = async (req, res, next) => {
   }
 };
 
-module.exports = createSerie;
+const updateSerie = async (req, res, next) => {
+  const serie = req.body;
+  try {
+    const updatedSerie = await Serie.findByIdAndUpdate(serie.id, serie, {
+      new: true,
+    });
+    if (updatedSerie) {
+      res.json(updatedSerie);
+    } else {
+      const error = new Error("Serie not found");
+      error.code = 404;
+      next(error);
+    }
+  } catch {
+    const error = new Error("Wrong id format");
+    error.code = 400;
+    next(error);
+  }
+};
+
+module.exports = { createSerie, updateSerie };
